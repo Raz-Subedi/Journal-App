c6d19e8dd623c2bc70c6831e96eb6bfacf995db8
Raz Subedi <subedirazkumar@gmail.com>
2024-12-16 16:35:12 +0545

    Initial commit

---
+++ journalApp/src/main/java/net/engineeringdigest/journalApp/entity/JournalEntry.java
@@ -0,0 +1,50 @@
+package net.engineeringdigest.journalApp.entity;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Document(collection = "journal_entries")   // mapped to Mongodb Collection
+            // journal entry's instance is equals to the row
+public class JournalEntry {       // POJO -> Plain Old Java Object
+    @Id  // unique key primary key
+    private ObjectId id;
+    private String title;
+    private String content;
+    private LocalDateTime date;
+
+    public ObjectId getId() {
+        return id;
+    }
+
+    public void setId(ObjectId id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public LocalDateTime getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDateTime date) {
+        this.date = date;
+    }
+}
